21d68d9ae6b97f766ed6b4150da3af8438fb689e
alexguimenti <alexguimenti@gmail.com>
2019-07-13 15:47:20 -0300

    add first comments

--- index.js
+++ index.js
@@ -2,28 +2,33 @@ const puppeteer = require("puppeteer");
 const credentials = require("./credentials");
 
 (async () => {
+  // startup puppeteer
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+
+  // go to panel
   await page.goto("https://panel.soclminer.com.br");
   const nav = new Promise(res => browser.on("targetcreated", res));
-  await Promise.all([
-    page.waitForSelector("#fbLogin"),
-    console.log(`page url: ${page.url()}`),
-    page.click("#fbLogin")
-  ]);
+
+  // click loggin button
+  await Promise.all([page.waitForSelector("#fbLogin"), page.click("#fbLogin")]);
   await nav;
-  await browser.pages(); // get all open pages by the browser
+  await browser.pages();
   const pages = await browser.pages();
+
+  // select popup window
   const popup = pages[pages.length - 1];
-  await Promise.all([
-    popup.waitForSelector("#email"),
-    console.log(`popup url: ${popup.url()}`)
-  ]);
+
+  // enter credentials and login
+  await Promise.all([popup.waitForSelector("#email")]);
   await popup.type("#email", credentials.email);
   await popup.type("#pass", credentials.pass);
   await popup.click("#loginbutton");
 
+  // wait to panel to load
   await page.waitFor("#bt-menu");
+
+  // choose customer
   await page.goto(
     "https://panel.soclminer.com.br/app/f6ff038c-24ee-43d4-b624-7fd945bb6929"
   );
