ba082f74b8d5ed1db6c8ffdf9b0e1ce4fd7ef172
alexguimenti <alexguimenti@gmail.com>
2019-07-13 15:45:13 -0300

    add automatic facebook login

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const puppeteer = require("puppeteer");
+const credentials = require("./credentials");
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://panel.soclminer.com.br");
-  await Promise.all([page.waitForSelector("#fbLogin"), page.click("#fbLogin")]);
+  const nav = new Promise(res => browser.on("targetcreated", res));
+  await Promise.all([
+    page.waitForSelector("#fbLogin"),
+    console.log(`page url: ${page.url()}`),
+    page.click("#fbLogin")
+  ]);
+  await nav;
   await browser.pages(); // get all open pages by the browser
+  const pages = await browser.pages();
+  const popup = pages[pages.length - 1];
+  await Promise.all([
+    popup.waitForSelector("#email"),
+    console.log(`popup url: ${popup.url()}`)
+  ]);
+  await popup.type("#email", credentials.email);
+  await popup.type("#pass", credentials.pass);
+  await popup.click("#loginbutton");
+
   await page.waitFor("#bt-menu");
-  console.log("loaded!");
   await page.goto(
     "https://panel.soclminer.com.br/app/f6ff038c-24ee-43d4-b624-7fd945bb6929"
   );
 
   await page.screenshot({ path: "example.png" });
-
-  // bt-menu
-  // https://panel.soclminer.com.br/app/f6ff038c-24ee-43d4-b624-7fd945bb6929
   // await browser.close();
 })();
