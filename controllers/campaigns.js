8b1949146082e270a571ffcb08872d20dfb52869
mexSocialMiner <alexandre.oliveira@socialminer.com>
2019-07-16 09:41:24 -0300

    add controllers folder

---
+++ controllers/campaigns.js
@@ -0,0 +1,23 @@
+// campaings array
+const campaigns = [
+    {
+        customer: "f6ff038c-24ee-43d4-b624-7fd945bb6929",
+        name: "nome1",
+        audience: "ab837adb-76e2-44ae-b981-31e56b35553d",
+        link: "http://www.uol.com.br",
+        start: "25/07/2019 14:50",
+        title: "Título da campanha 1",
+        message: "Mensagem da campanha 1"
+    },
+    {
+        customer: "f6ff038c-24ee-43d4-b624-7fd945bb6929",
+        name: "nome2",
+        audience: "ab837adb-76e2-44ae-b981-31e56b35553d",
+        link: "http://www.g1.com.br",
+        start: "26/08/2019 14:50",
+        title: "Título da campanha 2",
+        message: "Mensagem da campanha 2"
+    }
+];
+
+module.exports = campaigns;
--- index.js
+++ index.js
@@ -1,27 +1,9 @@
 const puppeteer = require("puppeteer");
-const credentials = require("./credentials");
+const credentials = require("./controllers/credentials");
+const campaignsArray = require("./controllers/credentials")
 
 // campaings array
-const campaigns = [
-  {
-    customer: "f6ff038c-24ee-43d4-b624-7fd945bb6929",
-    name: "nome1",
-    audience: "ab837adb-76e2-44ae-b981-31e56b35553d",
-    link: "http://www.uol.com.br",
-    start: "25/07/2019 14:50",
-    title: "Título da campanha 1",
-    message: "Mensagem da campanha 1"
-  },
-  {
-    customer: "f6ff038c-24ee-43d4-b624-7fd945bb6929",
-    name: "nome2",
-    audience: "ab837adb-76e2-44ae-b981-31e56b35553d",
-    link: "http://www.g1.com.br",
-    start: "26/08/2019 14:50",
-    title: "Título da campanha 2",
-    message: "Mensagem da campanha 2"
-  }
-];
+const campaigns = campaignsArray.campaigns;
 
 (async () => {
   // start puppeteer
