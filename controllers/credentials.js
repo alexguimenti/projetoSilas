7af0b138306bd54e435fa8839420ef82f01033f2
mexSocialMiner <alexandre.oliveira@socialminer.com>
2019-07-16 11:42:07 -0300

    add initial check function

--- controllers/credentials.js
+++
@@ -1,8 +0,0 @@
-// facebook password!
-
-const credentials = {
-    email: "FACEBOOK_EMAIL",
-    pass: "PASSWORD"
-};
-
-module.exports = credentials;
--- index.js
+++ index.js
@@ -2,17 +2,27 @@ const puppeteer = require("puppeteer");
 const CreateCampaignController = require("./controllers/CreateCampaignController");
 const FacebookLoginController = require("./controllers/FacebookLoginController");
 
-(async () => {
-  // start puppeteer
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  // go to panel
-  await page.goto("https://panel.soclminer.com.br");
-  await FacebookLoginController.facebookLogin(browser, page);
-  // wait panel to load
-  await page.waitFor("#bt-menu");
+const types = ["campanhas", "onsites", "bounces", "optins"]
 
-  await CreateCampaignController.campaignCreator(page)
+if (!types.includes(process.argv[2])) {
+  console.log("Entrar com uma das opções: 'campanhas', 'onsites', 'bounces', ou 'optins'!");
+}
+else {
+  (async () => {
 
-  // await browser.close();
-})();
+    // start puppeteer
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    // go to panel
+    await page.goto("https://panel.soclminer.com.br");
+    await FacebookLoginController.facebookLogin(browser, page);
+    // wait panel to load
+    await page.waitFor("#bt-menu");
+    if (!process.argv[2] === "campanhas") {
+      await CreateCampaignController.campaignCreator(page)
+    } else {
+      console.log(`Script para ${process.argv[2]} ainda não está disponível!`);
+      await browser.close();
+    }
+  })()
+};
