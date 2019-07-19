eb695757d4b2b9b06f9ebf8ef5604a9ece31d965
Alexandre Guimenti <alexguimenti@gmail.com>
2019-07-19 19:00:38 -0300

    add init() and exit function

--- index.js
+++ index.js
@@ -7,29 +7,39 @@ var rl = readline.createInterface(process.stdin, process.stdout);
 
 console.log("");
 console.log("Digite o código para o tipo de campanha/plugin você quer subir:");
-console.log("");
-console.log("[1] - Campanhas Manuais");
-console.log("[2] - Optins");
-console.log("");
 
-rl.question("-> ", answer => {
-  if (answer == "1" || answer == "2") {
-    (async () => {
-      // start puppeteer
-      const browser = await puppeteer.launch({ headless: false });
-      const page = await browser.newPage();
-      // go to panel
-      await page.goto("https://panel.soclminer.com.br");
-      await FacebookLoginController.facebookLogin(browser, page);
-      // wait panel to load
-      await page.waitFor("#bt-menu");
+init();
 
-      optionCheck(answer, browser);
-    })();
-  } else {
-    console.log("Opção Inválida!");
-  }
-});
+async function init() {
+  console.log("");
+  console.log("[1] - Campanhas Manuais");
+  console.log("[2] - Optins");
+  console.log("[Exit] - Encerrar programa");
+  console.log("");
+  rl.question("-> ", answer => {
+    if (answer == "1" || answer == "2") {
+      (async () => {
+        // start puppeteer
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        // go to panel
+        await page.goto("https://panel.soclminer.com.br");
+        await FacebookLoginController.facebookLogin(browser, page);
+        // wait panel to load
+        await page.waitFor("#bt-menu");
+
+        optionCheck(answer, browser);
+      })();
+    } else if (answer == "exit" || answer == "Exit") {
+      process.exit();
+    } else {
+      console.log("");
+      console.log("Opção Inválida! Tente de novo...");
+      console.log("");
+      init();
+    }
+  });
+}
 
 //const types = ["campanhas", "onsites", "bounces", "optins"]
 async function optionCheck(option, browser) {
