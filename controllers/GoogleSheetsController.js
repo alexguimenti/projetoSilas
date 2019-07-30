3cd54d7ebc1a67c366138e5138981909f3767971
mexSocialMiner <alexandre.oliveira@socialminer.com>
2019-07-30 15:17:41 -0300

    fix to get new worksheet

--- controllers/GoogleSheetsController.js
+++ controllers/GoogleSheetsController.js
@@ -8,15 +8,14 @@ GoogleSheetsController = {
     const doc = new GoogleSpreadSheet(
       "1qFJzC_RkAjwMLu2xFMmZORFAoFW0AtHByDlSkbrJ1X4"
     );
-    console.log(`answer2: ${amount}`);
     await promisify(doc.useServiceAccountAuth)(creds);
     const info = await promisify(doc.getInfo)();
-    const campaingsSheet = info.worksheets[0];
+    const campaingsSheet = info.worksheets[1];
     const cells = await promisify(campaingsSheet.getCells)({
       "min-row": 2,
       "max-row": 2 + (amount - 1),
-      "min-col": 12,
-      "max-col": 12
+      "min-col": 13,
+      "max-col": 13
     });
     for (const cell of cells) {
       let campaign = JSON.parse(cell.value);
